3b669efad815517137de204b320ddd993335e7b5
Lucas Rango <lucas.rango@gmail.com>
2020-06-13 11:36:26 -0300

    Initial commit w/Spring Initializr

---
+++ src/main/java/com/example/pokedemo/PokedemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pokedemo/PokedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
